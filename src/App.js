892b2f01922309cb2d5bd201654825e7745e1105
mattc-27 <mattcopeland927@gmail.com>
2023-11-25 15:55:29 -0700

    Add mobile layout 203-11-25T22:00Z≈

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import React from 'react';
 import Layout from './components/Layout';
 import Dashboard from './Dashboard';
 
-
 // stylesheet
 import './style.css';
-
-
-
 
 export default function App() {
     return (
