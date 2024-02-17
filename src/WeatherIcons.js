741533d745a567f050533df353bfc8783f40471d
mattc-27 <108201614+mattc-27@users.noreply.github.com>
2024-02-17 13:50:09 -0700

    Updates (#8)
    * Add mobile layout 203-11-25T22:00Z≈
    
    * Add mobile layout 2023-11-25T22:00Z (#3)
    
    * 20240209t205052 (#5)
    
    * v20240216t082006

--- src/WeatherIcons.js
+++ src/WeatherIcons.js
@@ -5,51 +5,51 @@ const WeatherIcons = [
 	{
 		code: 1000,
 		text: "Sunny",
-		value: <WiDaySunny size={80} style={{ color: "white" }} />,
-		forecast: <WiDaySunny size={45} style={{ color: "#252525" }} />,
+		value: <WiDaySunny size={120} style={{ color: "white"}} />,
+		forecast: <WiDaySunny size={70} style={{ color: "#fcfcfc", margin: '0.5%' }} />,
 		daily: <WiDaySunny size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1003,
 		text: "Partly cloudy",
-		value: <WiDayCloudy size={80} style={{ color: "white" }} />,
-		forecast: <WiDayCloudy size={45} style={{ color: "#252525" }} />,
+		value: <WiDayCloudy size={120} style={{ color: "white" }} />,
+		forecast: <WiDayCloudy size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiDayCloudy size={80} style={{ color: "white" }} />
 
 	},
 	{
 		code: 1006,
 		text: "Cloudy",
-		value: <WiDayCloudy size={80} style={{ color: "white" }} />,
-		forecast: <WiDayCloudy size={45} style={{ color: "#252525" }} />,
-		daily: <WiDayCloudy size={80} style={{ color: "white" }} />
+		value: <WiDaySunny size={120} style={{ color: "white" }} />,
+		forecast: <WiDaySunny size={60} style={{ color: "#fcfcfc" }} />,
+		daily: <WiDaySunny size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1009,
 		text: "Overcast",
-		value: <WiFog size={80} style={{ color: "white" }} />,
-		forecast: <WiFog size={45} style={{ color: "#252525" }} />,
+		value: <WiFog size={120} style={{ color: "white" }} />,
+		forecast: <WiFog size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiFog size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1030,
 		text: "Mist",
-		value: <WiFog size={80} style={{ color: "white" }} />,
-		forecast: <WiFog size={45} style={{ color: "#252525" }} />,
+		value: <WiFog size={120} style={{ color: "white" }} />,
+		forecast: <WiFog size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiFog size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1063,
 		text: "Patchy rain possible",
-		value: <WiRain size={80} style={{ color: "white" }} />,
-		forecast: <WiRain size={45} style={{ color: "#252525" }} />,
+		value: <WiRain size={120} style={{ color: "white" }} />,
+		forecast: <WiRain size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiRain size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1066,
 		text: "Patchy snow possible",
-		value: <WiSnowWind size={80} style={{ color: "white" }} />,
-		forecast: <WiSnowWind size={45} style={{ color: "#252525" }} />,
+		value: <WiSnowWind size={120} style={{ color: "white" }} />,
+		forecast: <WiSnowWind size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiSnowWind size={80} style={{ color: "white" }} />
 	},
 
@@ -57,140 +57,140 @@ const WeatherIcons = [
 		code: 1069,
 		text: "Patchy sleet possible",
 		value: <WiSleet size={80} style={{ color: "white" }} />,
-		forecast: <WiSleet size={45} style={{ color: "white" }} />,
+		forecast: <WiSleet size={60} style={{ color: "white" }} />,
 		daily: <WiSleet size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1072,
 		text: "Patchy freezing drizzle possible",
 		value: <WiSleet size={80} style={{ color: "white" }} />,
-		forecast: <WiSleet size={45} style={{ color: "#252525" }} />,
+		forecast: <WiSleet size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiSleet size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1087,
 		text: "Thundery outbreaks possible",
 		value: <WiDayThunderstorm size={80} style={{ color: "white" }} />,
-		forecast: <WiDayThunderstorm size={45} style={{ color: "#252525" }} />,
+		forecast: <WiDayThunderstorm size={60} style={{ color: "#fcfcfc" }} />,
 		daily:  <WiDayThunderstorm size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1114,
 		text: "Blowing snow",
 		value: <WiSnowWind size={80} style={{ color: "white" }} />,
-		forecast: <WiSnowWind size={45} style={{ color: "#252525" }} />,
+		forecast: <WiSnowWind size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiSnowWind size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1117,
 		text: "Blizzard",
 		value: <WiSnowWind size={80} style={{ color: "white" }} />,
-		forecast: <WiSnowWind size={45} style={{ color: "#252525" }} />,
+		forecast: <WiSnowWind size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiSnowWind size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1135,
 		text: "Fog",
 		value: <WiFog size={80} style={{ color: "white" }} />,
-		forecast: <WiFog size={45} style={{ color: "#252525" }} />,
+		forecast: <WiFog size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiFog size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1147,
 		text: "Freezing fog",
 		value: <WiFog size={80} style={{ color: "white" }} />,
-		forecast: <WiFog size={45} style={{ color: "#252525" }} />,
+		forecast: <WiFog size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiFog size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1100,
 		text: "Patchy light drizzle",
-		value: <WiRain size={80} style={{ color: "white" }} />,
-		forecast: <WiRain size={45} style={{ color: "#252525" }} />,
+		value: <WiRain size={120} style={{ color: "white" }} />,
+		forecast: <WiRain size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiRain size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1153,
 		text: "Light drizzle",
-		value: <WiRain size={80} style={{ color: "white" }} />,
-		forecast: <WiRain size={45} style={{ color: "#252525" }} />,
+		value: <WiRain size={120} style={{ color: "white" }} />,
+		forecast: <WiRain size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiRain size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1168,
 		text: "Freezing drizzle",
-		value: <WiRain size={80} style={{ color: "white" }} />,
-		forecast: <WiRain size={45} style={{ color: "#252525" }} />,
+		value: <WiRain size={120} style={{ color: "white" }} />,
+		forecast: <WiRain size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiRain size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1171,
 		text: "Heaving freezing drizzle",
-		value: <WiRain size={80} style={{ color: "white" }} />,
-		forecast: <WiRain size={45} style={{ color: "#252525" }} />,
+		value: <WiRain size={120} style={{ color: "white" }} />,
+		forecast: <WiRain size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiRain size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1180,
 		text: "Patchy light rain",
-		value: <WiRain size={80} style={{ color: "white" }} />,
-		forecast: <WiRain size={45} style={{ color: "#252525" }} />,
+		value: <WiRain size={120} style={{ color: "white" }} />,
+		forecast: <WiRain size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiRain size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1183,
 		text: "Light rain",
-		value: <WiRain size={80} style={{ color: "white", border: '1px solid #fff' }} />,
-		forecast: <WiRain size={45} style={{ color: "#252525" }} />,
+		value: <WiRain size={120}  />,
+		forecast: <WiRain size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiRain size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1186,
 		text: "Moderate rain at times",
-		value: <WiRain size={80} style={{ color: "white" }} />,
-		forecast: <WiRain size={45} style={{ color: "#252525" }} />,
+		value: <WiRain size={120} style={{ color: "white" }} />,
+		forecast: <WiRain size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiRain size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1189,
 		text: "Moderate rain",
 		value: <WiRain size={80} style={{ color: "white" }} />,
-		forecast: <WiRain size={45} style={{ color: "#252525" }} />,
+		forecast: <WiRain size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiRain size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1192,
 		text: "Heavy rain at times",
 		value: <WiRain size={80} style={{ color: "white" }} />,
-		forecast: <WiRain size={45} style={{ color: "#25252525" }} />,
+		forecast: <WiRain size={60} style={{ color: "#fcfcfc25" }} />,
 		daily: <WiRain size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1195,
 		text: "Heavy rain",
 		value: <WiRain size={80} style={{ color: "white" }} />,
-		forecast: <WiRain size={45} style={{ color: "#25252525" }} />,
+		forecast: <WiRain size={60} style={{ color: "#fcfcfc25" }} />,
 		daily: <WiRain size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1198,
 		text: "Light freezing rain",
 		value: <WiRain size={80} style={{ color: "white" }} />,
-		forecast: <WiRain size={45} style={{ color: "#252525" }} />,
+		forecast: <WiRain size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiRain size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1201,
 		text: "Moderate or heavy freezing rain",
 		value: <WiRain size={80} style={{ color: "white" }} />,
-		forecast: <WiRain size={45} style={{ color: "#252525" }} />,
+		forecast: <WiRain size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiRain size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1204,
 		text: "Light sleet",
 		value: <WiSleet size={80} style={{ color: "white" }} />,
-		forecast: <WiSleet size={45} style={{ color: "#252525" }} />,
+		forecast: <WiSleet size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiSleet size={80} style={{ color: "white" }} />
 
 	},
@@ -198,7 +198,7 @@ const WeatherIcons = [
 		code: 1207,
 		text: "Moderate or heavy sleet",
 		value: <WiSleet size={80} style={{ color: "white" }} />,
-		forecast: <WiSleet size={45} style={{ color: "#252525" }} />,
+		forecast: <WiSleet size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiSleet size={80} style={{ color: "white" }} />
 
 	},
@@ -206,7 +206,7 @@ const WeatherIcons = [
 		code: 1210,
 		text: "Patchy light snow",
 		value: <WiSnowWind size={80} style={{ color: "white" }} />,
-		forecast: <WiSnowWind size={45} style={{ color: "#252525" }} />,
+		forecast: <WiSnowWind size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiSnowWind size={80} style={{ color: "white" }} />
 
 	},
@@ -214,133 +214,133 @@ const WeatherIcons = [
 		code: 1213,
 		text: "Light snow",
 		value: <WiSnowWind size={80} style={{ color: "white" }} />,
-		forecast: <WiSnowWind size={45} style={{ color: "#252525" }} />,
+		forecast: <WiSnowWind size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiSnowWind size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1216,
 		text: "Patchy moderate snow",
 		value: <WiSnowWind size={80} style={{ color: "white" }} />,
-		forecast: <WiSnowWind size={45} style={{ color: "#252525" }} />,
+		forecast: <WiSnowWind size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiSnowWind size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1219,
 		text: "Moderate snow",
 		value: <WiSnowWind size={80} style={{ color: "white" }} />,
-		forecast: <WiSnowWind size={45} style={{ color: "#252525" }} />,
+		forecast: <WiSnowWind size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiSnowWind size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1222,
 		text: "Patchy heavy snow",
 		value: <WiSnowWind size={80} style={{ color: "white" }} />,
-		forecast: <WiSnowWind size={45} style={{ color: "#252525" }} />,
+		forecast: <WiSnowWind size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiSnowWind size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1225,
 		text: "Heavy snow",
 		value: <WiSnowWind size={80} style={{ color: "white" }} />,
-		forecast: <WiSnowWind size={45} style={{ color: "#252525" }} />,
+		forecast: <WiSnowWind size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiSnowWind size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1237,
 		text: "Ice pellets",
 		value: <WiSnowWind size={80} style={{ color: "white" }} />,
-		forecast: <WiSnowWind size={45} style={{ color: "#252525" }} />,
+		forecast: <WiSnowWind size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiSnowWind size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1240,
 		text: "Light rain shower",
 		value: <WiRain size={80} style={{ color: "white" }} />,
-		forecast: <WiRain size={45} style={{ color: "#252525" }} />,
+		forecast: <WiRain size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiRain size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1243,
 		text: "Moderate or heavy rain shower",
 		value: <WiRain size={80} style={{ color: "white" }} />,
-		forecast: <WiRain size={45} style={{ color: "#252525" }} />,
+		forecast: <WiRain size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiRain size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1246,
 		text: "Torrential rain shower",
 		value: <WiRain size={80} style={{ color: "white" }} />,
-		forecast: <WiRain size={45} style={{ color: "#252525" }} />,
+		forecast: <WiRain size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiRain size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1249,
 		text: "Light sleet showers",
 		value: <WiSleet size={80} style={{ color: "white" }} />,
-		forecast: <WiSleet size={45} style={{ color: "#252525" }} />,
+		forecast: <WiSleet size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiSleet size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1252,
 		text: "Moderate or heavy sleet showers",
 		value: <WiSleet size={80} style={{ color: "white" }} />,
-		forecast: <WiSleet size={45} style={{ color: "#252525" }} />,
+		forecast: <WiSleet size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiSleet size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1255,
 		text: "Light snow showers",
 		value: <WiSnowWind size={80} style={{ color: "white" }} />,
-		forecast: <WiSnowWind size={45} style={{ color: "#252525" }} />,
+		forecast: <WiSnowWind size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiSnowWind size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1258,
 		text: "Moderate or heavy snow showers",
 		value: <WiSnowWind size={80} style={{ color: "white" }} />,
-		forecast: <WiSnowWind size={45} style={{ color: "#252525" }} />,
+		forecast: <WiSnowWind size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiSnowWind size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1261,
 		text: "Light showers of ice pellets",
 		value: <WiSleet size={80} style={{ color: "white" }} />,
-		forecast: <WiSleet size={45} style={{ color: "#252525" }} />,
+		forecast: <WiSleet size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiRain size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1264,
 		text: "Moderate or heavy showers of ice pellets",
 		value: <WiSnowWind size={80} style={{ color: "white" }} />,
-		forecast: <WiSnowWind size={45} style={{ color: "#252525" }} />,
+		forecast: <WiSnowWind size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiSnowWind size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1273,
 		text: "Patchy light rain with thunder",
 		value: <WiDayThunderstorm size={80} style={{ color: "white" }} />,
-		forecast: <WiDayThunderstorm size={45} style={{ color: "#252525" }} />,
+		forecast: <WiDayThunderstorm size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiDayThunderstorm size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1276,
 		text: "Moderate or heavy rain with thunder",
 		value: <WiDayThunderstorm size={80} style={{ color: "white" }} />,
-		forecast: <WiDayThunderstorm size={45} style={{ color: "#252525" }} />,
+		forecast: <WiDayThunderstorm size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiDayThunderstorm size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1279,
 		text: "Patchy light snow with thunder",
 		value: <WiDaySnowThunderstorm size={80} style={{ color: "white" }} />,
-		forecast: <WiDaySnowThunderstorm size={45} style={{ color: "#252525" }} />,
+		forecast: <WiDaySnowThunderstorm size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiDaySnowThunderstorm size={80} style={{ color: "white" }} />
 	},
 	{
 		code: 1282,
 		text: "Moderate or heavy snow with thunder",
 		value: <WiDaySnowThunderstorm size={80} style={{ color: "white" }} />,
-		forecast: <WiDaySnowThunderstorm size={45} style={{ color: "#252525" }} />,
+		forecast: <WiDaySnowThunderstorm size={60} style={{ color: "#fcfcfc" }} />,
 		daily: <WiDaySnowThunderstorm size={80} style={{ color: "white" }} />
 	}
 ];
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,120 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Outlet } from 'react-router-dom';
+import toast, { Toaster } from 'react-hot-toast';
+import Backgrounds from '../Backgrounds';
+import SearchComponent from './SearchComponent';
+import Footer from './Footer';
+import CurrentWeather from '../CurrentWeather';
+import { WeatherContext } from "../weatherContext";
+import { getWeatherData, getRandomItem } from '../weatherServices';
 
 export default function Layout() {
 
+    const [query, setQuery] = useState({ q: '' });
+    const [weather, setWeather] = useState('');
+
+    const { currentConditions, setCurrentConditions, getWeather, getBackground, background, setBackground } = useContext(WeatherContext);
+
+    useEffect(() => {
+        async function getUserLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(success, error);
+            } else {
+                //setQuery({q: 'San Francisco'})
+                console.log('Geolocation not suppoerted')
+            }
+        }
+        function success(position) {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            setQuery({ q: `${latitude},${longitude}` })
+            console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+        }
+        function error() {
+            //setQuery({q: 'San Francisco'})
+            console.log('Unable to retrieve your location')
+        }
+        getUserLocation()
+    }, [])
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await toast.promise(getWeatherData(query), {
+                    loading: 'Coming up with weather puns is a breeze.',
+                    success: 'Coming up with weather puns is a breeze.',
+                    error: 'Error when fetching',
+                },
+                    {
+                        style: {
+                            minWidth: '270px',
+                            height: '120px',
+                            background: '#fafafa',
+                            color: '#252525',
+                            fontFamily: 'Lato',
+                            fontSize: '1.5em',
+                            fontWeight: '500',
+                            textAlign: 'center',
+
+                        },
+                        success: {
+                            duration: 2000,
+                            icon: null
+                        },
+                    }
+                )
+                // set weather data
+                console.log(response)
+                setCurrentConditions(response)
+
+                const currentLocation = Backgrounds.images.find(({ region }) => region === response.region)
+
+                console.log(currentLocation)
+                await getBackground(currentLocation)
+
+            } catch (error) {
+                console.error(error.message)
+            }
+        }
+        fetchData()
+            .catch(console.error);
+    }, [query]);
+
+    const getIcon = (code) => {
+        const iconData = WeatherIcons.find(({ code: iconCode }) => iconCode === code);
+        return iconData ? iconData.forecast : null;
+    };
+
+    const [tempF, setTempF] = useState(true)
+
+    function changeTemp() {
+        if (!tempF) {
+            setTempF(true)
+        } else setTempF(false)
+    }
+
+    function Loading() {
+        return (
+            <div className='loading' style={{ fontFamily: 'Lato', fontSize: '1.5em', fontWeight: '300' }}>
+                <p><i>Coming up with weather puns is a breeze.</i></p>;
+            </div>
+        )
+    }
+
     return (
-        <Outlet />
+        <>
+            <SearchComponent setQuery={setQuery} />
+            <Toaster containerStyle={{
+                position: 'absolute',
+                zIndex: '999999999999999999',
+                top: '5%'
+            }} />
+            {currentConditions ?
+                <CurrentWeather currentConditions={currentConditions} />
+                : null
+            }
+            <Footer />
+        </>
     );
 }
